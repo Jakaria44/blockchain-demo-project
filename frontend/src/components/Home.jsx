853a4b4b68b9c7e12351ef5ec4ea6d66cf3d2e94
Jakaria44 <mdjakaria442020@gmail.com>
2023-07-12 12:25:35 +0600

    modified Home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,13 +5,7 @@ import { useContract, useContractRead } from "@thirdweb-dev/react";
 import { useEffect } from "react";
 
 const Home = () => {
-  
 
-  const { contract } = useContract("0x5A34F8dF70bBa6bbED9e736aa6883Ff2ff324Ed2");
-  const { data, isLoading } = useContractRead(contract, "getWinner");
-  const clicked= ()=>{
-    console.log(data);
-  }
   return (
     <div>
       <ConnectWallet
@@ -22,7 +16,6 @@ const Home = () => {
         theme="dark"
         btnTitle="Connect Wallet"
       />
-      <button onClick={clicked}>Connect</button>
       
     </div>
   );
